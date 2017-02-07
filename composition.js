2975c180c03e7e2ec15ba622f3bad73ec0160d78
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-07 20:34:12 +0700

    refaktor lagi

--- composition.js
+++ composition.js
@@ -1,57 +1,3 @@
-class CookiesFactory{
-  constructor(){
-    this.lists = []
-  }
-
-  changetoObj(arr){
-    let newArr = []
-    for (let i = 0; i < arr.length; i++) {
-
-    }
-    return
-  }
-
-  create(option){
-    for(let i=0; i<option.length; i++){
-
-      let list = option[i].split("=")
-      let nameOfCookie = list[0].trim()// nama kue
-      let ingredients = list[1].trim().split(",")// nama2 bahan
-      let bahan = [] // bahan dalam bentuk objek?
-      let sugarFree  = false
-
-      for (let j=0; j < ingredients.length; j++) {
-        if(ingredients[j].includes('sugar')){
-          sugarFree  = true
-        }else{
-          sugarFree  = false
-        }
-        ingredients[j] = ingredients[j].split(":")
-        bahan.push(new Ingredients({
-          'name' : ingredients[j][1].trim(),
-          'amount' : ingredients[j][0].trim(),
-          'has_sugar' : sugarFree,
-
-        }))
-      }
-
-      switch (nameOfCookie) {
-        case "choco chip":
-          this.lists.push(new ChocolateChip(nameOfCookie,bahan))
-          break;
-        case "peanut butter":
-          this.lists.push(new PeanutButter(nameOfCookie,bahan))
-        break;
-        default:
-          this.lists.push(new OtherCookie(nameOfCookie,bahan))
-      }
-
-    }
-
-    return this.lists// isi array masing2 mentah dari baca data txt
-  }
-}
-
 class Cookies{
   constructor(nameOfCookie,ingredients){
     this.name = nameOfCookie
@@ -94,13 +40,49 @@ class OtherCookie extends Cookies{
   }
 }
 
-// class PlainCookies extends Cookies{
-//   constructor(){
-//     super()
-//     this.name = "Plain"
-//     this.cooked_time = 30
-//   }
-// }
+class CookiesFactory{
+  constructor(){
+    this.lists = []
+  }
+
+  create(option){
+    for(let i=0; i<option.length; i++){
+
+      let list = option[i].split("=")
+      let nameOfCookie = list[0].trim()// nama kue
+      let ingredients = list[1].trim().split(",")// nama2 bahan
+      let bahan = [] // bahan dalam bentuk objek?
+      let sugarFree  = false
+
+      for (let j=0; j < ingredients.length; j++) {
+        if(ingredients[j].includes('sugar')){
+          sugarFree  = true
+        }else{
+          sugarFree  = false
+        }
+        ingredients[j] = ingredients[j].split(":")
+        bahan.push(new Ingredients({
+          'name' : ingredients[j][1].trim(),
+          'amount' : ingredients[j][0].trim(),
+          'has_sugar' : sugarFree,
+
+        }))
+      }
+
+      switch (nameOfCookie) {
+        case "choco chip":
+          this.lists.push(new ChocolateChip(nameOfCookie,bahan))
+          break;
+        case "peanut butter":
+          this.lists.push(new PeanutButter(nameOfCookie,bahan))
+        break;
+        default:
+          this.lists.push(new OtherCookie(nameOfCookie,bahan))
+      }
+    }
+    return this.lists
+  }
+}
 
 let fs = require('fs')
 let data = fs.readFileSync("cookies.txt","utf-8").trim().split("\n")
