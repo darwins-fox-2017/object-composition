037c16e8a6afe797e3fa02e701b19bdc7c1fe6da
Eri Irawan <eryirawan91@gmail.com>
2017-02-07 22:23:31 +0700

    supposed to be done

--- composition.js
+++ composition.js
@@ -1,5 +1,6 @@
 "use strict"
-const fs      = require('fs')
+const fs        = require('fs')
+const readFile  = fs.readFileSync('cookies.txt', 'utf-8').trim().split('\n')
 
 class Cookies {
   constructor(nama, ingredients) {
@@ -9,54 +10,57 @@ class Cookies {
 }
 
 class Ingredients {
-  constructor(nama, amount) {
+  constructor(nama, amount, has_sugar) {
     this.nama   = nama
     this.amount = amount
+    this.sugar  = has_sugar
   }
 }
 
 class PeanutButter extends Cookies {
   constructor(nama, ingredients) {
-    super(name, ingredients)
+    super(nama, ingredients)
+    this.peanut_count = 100
   }
 }
 
 class ChocolateChip extends Cookies {
   constructor(nama, ingredients) {
     super(nama, ingredients)
+    this.choc_chip = 200
   }
 }
 
-class ChocolateCheese extends Cookies {
+class OtherCookies extends Cookies {
   constructor(nama, ingredients) {
     super(nama, ingredients)
-  }
-}
-
-class ChocolateButter extends Cookies {
-  constructor(nama, ingredients) {
-    super(nama, ingredients)
+    this.choc_chesse = 150
   }
 }
 
 function cookiesFactory(arr) {
-  let num = []
+  var num = []
 
   for (let i = 0; i < arr.length; i++) {
-    num.push(arr[i].split(' : '))
+    num.push(arr[i].split(':'))
   }
 
-  let getA   = [],
-      getB   = [],
-      object = [];
+  let getA      = [],
+      getB      = [],
+      object    = [],
+      sugarFree = false
 
   for (let i = 0; i < num.length; i++) {
     getA.push(num[i][0])
+    if (num[i][1].includes("sugar")) {
+      sugarFree = true
+    }
     getB.push(num[i][1])
   }
-
+  // console.log("Punya A "+getA);
+  // console.log("Punya B "+getB);
   for (let i = 0; i < getA.length; i++) {
-    object.push(new Ingredients(getB[i], getA[i]))
+    object.push(new Ingredients(getB[i], getA[i], sugarFree))
   }
   return object
 }
@@ -67,14 +71,13 @@ class CookieFactory {
   }
 
   create(option){
-    let readFile    = fs.readFileSync('cookies.txt', 'utf-8').trim().split('\n')
     let resultSplit = [],
         cookies     = [],
         ingredients = [],
         composition = [];
 
     for (let i = 0; i < readFile.length; i++) {
-      resultSplit.push(readFile[i].split('='))
+      resultSplit.push(readFile[i].trim().split(' = '))
     }
 
     for (let i = 0; i < resultSplit.length; i++) {
@@ -85,7 +88,7 @@ class CookieFactory {
     for (let i = 0; i < ingredients.length; i++) {
     composition.push(ingredients[i].split(','))
     }
-
+    console.log(cookies);
     for (let i = 0; i < cookies.length; i++) {
       if (cookies[i] == "peanut butter") {
         let resultPeanut = cookiesFactory(composition[i])
@@ -93,12 +96,9 @@ class CookieFactory {
       }else if (cookies[i] == "chocolate chip") {
         let resultChip  = cookiesFactory(composition[i])
         this.result.push(new ChocolateChip(cookies[i], resultChip))
-      }else if (cookies[i] == "chocolate cheese") {
-        let resultCheese  = cookiesFactory(composition[i])
-        this.result.push(new ChocolateCheese(cookies[i], resultCheese))
-      }else if (cookies[i] == "chocolate butter") {
-        let resultButter  = cookiesFactory(composition[i])
-        this.result.push(new ChocolateButter(cookies[i], resultButter))
+      }else{
+        let resultOther  = cookiesFactory(composition[i])
+        this.result.push(new OtherCookies(cookies[i], resultOther))
       }
     }//end for cookies
     return this.result
